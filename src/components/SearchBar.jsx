7fe50064a087fb0b156681eef83231dbdc87f97d
Damjan15 <stamenovdamjan20@gmail.com>
2022-11-17 20:32:11 +0100

    Search functionality added, search feed rendering searched videos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,15 +4,27 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material"; 
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  }
   return (
-    <Paper component="form" onSubmit={() => {}} sx={{
+    <Paper component="form" onSubmit={onHandleSubmit} sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
         pl: 2,
         boxShadow: 'none',
         mr: { sm: 5 }
     }}>
-        <input placeholder="Search..." className="search-bar" value="" onChange={() => {}} />
+        <input placeholder="Search..." className="search-bar" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
 
         <IconButton type="submit" sx={{ p: '10px', color: 'red'}}>
             <Search />
